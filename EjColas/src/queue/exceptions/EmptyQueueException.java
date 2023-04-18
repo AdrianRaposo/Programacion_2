ea15d6b98588f658a2da507fbf0539f2c48003d8
AdrianRaposo <92587367+AdrianRaposo@users.noreply.github.com>
2023-04-18 21:59:59 +0200

    Delete EjColas directory

--- EjColas/src/queue/exceptions/EmptyQueueException.java
+++
@@ -1,9 +0,0 @@
-package queue.exceptions;
-
-public class EmptyQueueException extends Exception {
-	public EmptyQueueException() {};
-	public EmptyQueueException(String msg) {
-		super(msg);
-	}
-	
-}
