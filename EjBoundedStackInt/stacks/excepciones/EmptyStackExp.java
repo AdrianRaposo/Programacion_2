6e98d36996ff1ca34dde472e7de9c34c2bee4f76
AdrianRaposo <92587367+AdrianRaposo@users.noreply.github.com>
2023-04-18 21:59:50 +0200

    Delete EjBoundedStackInt directory

--- EjBoundedStackInt/stacks/excepciones/EmptyStackExp.java
+++
@@ -1,7 +0,0 @@
-package stacks.excepciones;
-
-public class EmptyStackExp extends Exception {
-	public EmptyStackExp(String m) {
-		super(m);
-	}
-}
--- EjBoundedStackInt/stacks/excepciones/FullStackExp.java
+++
@@ -1,7 +0,0 @@
-package stacks.excepciones;
-
-public class FullStackExp extends Exception {
-	public FullStackExp(String m) {
-		super(m);
-	}
-}
